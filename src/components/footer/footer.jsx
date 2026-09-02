f3afb71de0af358a8f2f4693aeecf0ef4bad9c36
Hamish-Bell <hamish@example.com>
2026-09-02 12:32:33 +0930

    fix footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,14 +1,16 @@
-import React, { useState } from 'react';
-import './footer.css'
+import React from 'react';
+import './footer.css';
 
-function Footer(){
-    return(
-        <div className='footer'>
-            <h1 className='name'>Hamish Bell</h1>
-            <p className='location'>Based in Adelaide</p>
-            <p className='privacy'>© 2026 HamishBell.net | All rights reserved.</p>
+function Footer() {
+    return (
+        <div className="footer">
+            <h1 className="name">Hamish Bell</h1>
+            <p className="location">Based in Adelaide</p>
+            <p className="privacy">
+                © 2026 HamishBell.net | All rights reserved.
+            </p>
         </div>
-    );  
+    );
 }
 
-export default Footer
+export default Footer;
